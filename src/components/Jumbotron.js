7eefc36ebf9942e845186c6d400b80ad400b58b1
Link <username@users.noreply.github.com>
2019-03-14 18:36:01 -0700

    initial files

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron">
+      <h1>Clicky Game</h1>
+      <h4>
+        Click on each of the pictures below to increase your score. Do not click on the same picture more than once or you will lose!
+      </h4>
+    </div>
+    );
+}
+export default Jumbotron;
